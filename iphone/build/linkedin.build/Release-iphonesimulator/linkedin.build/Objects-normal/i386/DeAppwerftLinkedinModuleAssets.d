156d30a39c6fa121342a699b4617f064070e5468
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-07-07 12:20:34 +0200

    arm64

--- iphone/build/linkedin.build/Release-iphonesimulator/linkedin.build/Objects-normal/i386/DeAppwerftLinkedinModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/fuerst/Documents/APPC_WORKSPACE/Ti.SignInWith/iphone/Classes/DeAppwerftLinkedinModuleAssets.m \
-  /Users/fuerst/Documents/APPC_WORKSPACE/Ti.SignInWith/iphone/Classes/DeAppwerftLinkedinModuleAssets.h
